a9e38e63464abd18c90dfb5bb0f3fadd728e0a5b
Soul <S.soulSlasher@gmail.com>
2023-09-25 17:35:38 +0200

    Ouais ca marche (Fix Raycast)

--- Assets/Script/CarrionScript.cs
+++ Assets/Script/CarrionScript.cs
@@ -8,9 +8,8 @@ public class CarrionScript : MonoBehaviour
     public float speed = 5;
     public float distRaycast = 5;
     public int nbdeLiane = 6;
-    public float MaxDist = 5;
 
-    public List<RaycastHit2D> hit = new List<RaycastHit2D>(1);
+    public List<RaycastHit2D> hit = new List<RaycastHit2D>();
 
     void Start()
     {
@@ -20,33 +19,52 @@ public class CarrionScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += new Vector3(speed * Time.deltaTime,0,0);
-            Check();
+            //Check();
         }
         if (Input.GetKey(KeyCode.Q))
         {
             transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
-            Check();
+            //Check();
         }
 
+        hit.Clear();
+        hit = new List<RaycastHit2D>();
 
+        RaycastHit2D hitr = Physics2D.Raycast(transform.position, new Vector2(0, 1), distRaycast);
+        if (hitr) hit.Add(hitr);
+        hitr = Physics2D.Raycast(transform.position, new Vector2(-1, 1), distRaycast);
+        if (hitr) hit.Add(hitr);
+        hitr = Physics2D.Raycast(transform.position, new Vector2(1, 1)  , distRaycast);
+        if (hitr) hit.Add(hitr);
+        hitr = Physics2D.Raycast(transform.position, new Vector2(0, -1) , distRaycast);
+        if (hitr) hit.Add(hitr);
+        hitr = Physics2D.Raycast(transform.position, new Vector2(-1, -1), distRaycast);
+        if (hitr) hit.Add(hitr);
+        hitr = Physics2D.Raycast(transform.position, new Vector2(1, -1) , distRaycast);
+        if (hitr) hit.Add(hitr);
     }
-    void Check()
-    {
-        foreach(RaycastHit2D h in hit)
+    /*    void Check()
         {
-            if (Vector2.Distance(h.transform.position, transform.position) > MaxDist)
+            foreach(RaycastHit2D h in hit)
             {
-                hit.Remove(h);
-                ConnectOne();
+                if(h == false)
+                {
+                    break;
+                }
+                if (Vector2.Distance(h.transform.position, transform.position) > MaxDist)
+                {
+                    hit.Remove(h);
+                    ConnectOne();
+                }
             }
-        }
-    }
+        }*/
     void ConnectOne()
     {
-        int Angle = Random.Range(0, 361);
+/*        int Angle = Random.Range(0, 361);
         Vector2 polaire = new Vector2(Mathf.Cos(Angle), Mathf.Sin(Angle)) * distRaycast;
         hit.Add(Physics2D.Raycast(transform.position, polaire));
         if (hit[hit.Count - 1])
@@ -55,36 +73,37 @@ public class CarrionScript : MonoBehaviour
         }
         else
         {
-            ConnectOne();
-        }
+            //ConnectOne();
+        }*/
     }
     void ConnectAll()
     {
-        hit.Clear();
-        
+       /* hit.Clear();
+
         for (int i = 0; i < nbdeLiane; i++)
         {
-            hit.Add(Physics2D.Raycast(transform.position, Vector2.zero));
-            while (!hit[i])
+            //hit.Add(Physics2D.Raycast(transform.position, Vector2.zero));
+            int Angle = Random.Range(0, 361);
+            Vector2 polaire = new Vector2(Mathf.Cos(Angle), Mathf.Sin(Angle)) * distRaycast;
+            hit.Add(Physics2D.Raycast(transform.position, polaire));
+            if (hit[i])
             {
-                int Angle = Random.Range(0, 361);
-                Vector2 polaire = new Vector2(Mathf.Cos(Angle), Mathf.Sin(Angle)) * distRaycast;
-                hit.Add(Physics2D.Raycast(transform.position, polaire));
-                if (hit[i])
-                {
-                    print("Hit");
-                }
-            }
-        }
+                print("Hit");
+            }           
+        }*/
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = new Color(0, 255, 0, .6f);
-        Gizmos.DrawSphere(transform.position, MaxDist);
+        UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.back ,distRaycast);
 
         Gizmos.color = Color.red;
         for (int i = 0; i < hit.Count; i++)
         {
+            if (hit[i] == false)
+            {
+                break;
+            }
             Gizmos.DrawCube(hit[i].point, new Vector2(.3f, .3f));
         }
     }
