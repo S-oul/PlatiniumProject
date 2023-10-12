42efbfb5b240b5f89d228aea8acdc96c7f9fa656
Sacha (Pc) <S.soulslasher@gmail.com>
2023-10-12 11:33:37 +0200

    Fix Prefab + Tool Start

--- Assets/Scripts/Room/RuleInspector.cs
+++ Assets/Scripts/Room/RuleInspector.cs
@@ -5,43 +5,96 @@ using System;
 using Unity.VisualScripting;
 using UnityEditor.Timeline.Actions;
 using NaughtyAttributes.Editor;
+using Unity.VisualScripting.FullSerializer;
+using System.Collections.Generic;
 
 
 
 //[EditorWindowTitle(title ="Inspector Rule")]
 public class RuleInspector : EditorWindow
 {
-    public string myString = "Hello World";
+    #region Sizes
+    public Rect HeaderSize;
+    public Rect MiddleSize;
 
-    [SerializeField] public SerializedObject SelectedRules; 
-        //Selection.activeContext.GameObject().GetComponent<Rules>().ConvertTo<SerializedProperty>();
-    
-        
+    public Rect RoomSize;
 
+    public Rect ButtonAddSize;
+    public Rect ButtonRemoveSize;
+    #endregion
 
-    [MenuItem("Window/Room rule")]
+    private List<int> _roomList;
+
+    [MenuItem("Tools/Room rule")]
     public static void ShowWindow()
     {
-        GetWindow(typeof(RuleInspector));
+        RuleInspector window = (RuleInspector)GetWindow(typeof(RuleInspector));
+        window.minSize = new Vector2(800, 400);
+        window.Show();
     }
     void OnGUI()
     {
-        GUILayout.Label("Room Rule Settings \n", EditorStyles.boldLabel);
+        GuiLayout();
+        DrawHeader();
+        DrawMiddle();
+        //GUI.DragWindow GUI.DragWindowGUI.DragWindowGUI.DragWindow
+    }
+    private void GuiLayout()
+    {
+        HeaderSize = new Rect(0, 0, Screen.width, 50);
+        MiddleSize = new Rect(0,50, Screen.width, 300);
+        //new Rect(0,350,);
 
-        EditorGUILayout.ObjectField(SelectedRules.ConvertTo<SerializedProperty>());
-            
-            //(Resources.LoadAll("Assets/Resources/Prefabs/Rooms/").ConvertTo<SerializedObject>());
+        RoomSize = new Rect(0, MiddleSize.y + ButtonAddSize.y + 10, 150, 150);
 
-        //EditorGUILayout.ObjectField(SelectedRules);
-        EditorGUILayout.BeginToggleGroup("Optional Settings",true);
+        ButtonAddSize = new Rect(0 + 5, MiddleSize.y + 5, 75, 20);
+        ButtonRemoveSize = new Rect(80 + 5, MiddleSize.y + 5, 100, 20);
+
+    }
+    private void DrawHeader()
+    {
+        GUI.color = Color.white;
+        Texture2D texture = new Texture2D(1,1);
+        texture.Apply();
+        GUI.DrawTexture(HeaderSize, texture);
+    }
+    
+    private void DrawMiddle()
+    {
+        GUI.color = Color.grey;
+        Texture2D background = new Texture2D(1, 1);
+        background.Apply();
+        GUI.DrawTexture(MiddleSize, background);
+
+        bool addRoom = GUI.Button(ButtonAddSize, "Add Room");
+        if (addRoom) AddRoom();
+
+        bool removeRoom = GUI.Button(ButtonRemoveSize, "Remove Room");
+        if (removeRoom) RemoveRoom();
+
+        for(int i = 0; i < _roomList.Count; i++)
         {
-            //EditorGUILayout.ObjectField(SelectedRules);
-
-
+            GUI.color = Color.Lerp(Color.red, Color.blue, (float)i/_roomList.Count);
+            Texture2D texture = new Texture2D(1, 1);
+            texture.Apply();
+            RoomSize.x = i * RoomSize.size.x + 5*i;
+            GUI.DrawTexture(RoomSize, texture);
+            
         }
 
+    }
 
+    void AddRoom()
+    {
+        _roomList.Add(1);
+    }
+    void RemoveRoom()
+    {
+        _roomList.Remove(1);
+    }
 
-        EditorGUILayout.EndToggleGroup();
+    private void OnEnable()
+    {
+        _roomList = new List<int>();
     }
 }
