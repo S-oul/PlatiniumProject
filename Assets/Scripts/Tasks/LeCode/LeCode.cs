ade13f6ee0afb0c5ed8b8905881211f50087a653
Sacha (Pc) <S.soulslasher@gmail.com>
2023-10-26 17:36:52 +0200

    Fix All Rooms + Code(unfinshed)

--- Assets/Scripts/Tasks/LeCode/LeCode.cs
+++ Assets/Scripts/Tasks/LeCode/LeCode.cs
@@ -32,20 +32,22 @@ public class LeCode : Task
 
         print(_code);
         int r = Random.Range(0, _gameManager.RoomList.Count);
-        int count = 0;
-        while (_gameManager.RoomList[r].HasPostIt && count < 500)
+        while (_gameManager.RoomList[r].HasPostIt)
         {
             r = Random.Range(0, _gameManager.RoomList.Count);
-            count++;
-            if(count > 400) { print("PAS NORMALE"); }   
+        }
+        if(_gameManager.RoomList[r].PosItPos == null)
+        {
+            print(_gameManager.RoomList[r]);
+            //_gameManager.RoomList[r].gameObject.SetActive(false);
         }
             GameObject go = Instantiate(_postIt);
             go.transform.SetParent(_gameManager.RoomList[r].gameObject.transform);
             go.transform.localPosition = _gameManager.RoomList[r].PosItPos.localPosition;
             _gameManager.RoomList[r].HasPostIt = true;
-            //go.transform.localScale = new Vector3(.1f, .2f, 1);
-            go.transform.localScale = new Vector3(1 / transform.parent.transform.localScale.x, 1 / transform.parent.transform.localScale.y, 1 / transform.parent.transform.localScale.z);
-        go.GetComponent<PostIt>().Code = _code;
+            go.transform.localScale = new Vector3(.1f, .2f, 1);
+            //go.transform.localScale = new Vector3(1 / transform.parent.transform.localScale.x, 1 / transform.parent.transform.localScale.y, 1 / transform.parent.transform.localScale.z);
+            go.GetComponent<PostIt>().Code = _code;
             go.GetComponent<PostIt>().Initialize();
     }
 
