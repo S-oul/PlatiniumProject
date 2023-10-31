cf3d52853d33ebcf5ab84cf8a2cadbd23d7d9360
Alexis Le Bourhis <lebourhisalexis11@gmail.com>
2023-10-31 09:25:45 +0100

    Auto stash before merge of "Alexis" and "origin/Shane"

--- Assets/VolleyballTask.cs
+++ Assets/VolleyballTask.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class VolleyballTask : InputTask
 {
+    [SerializeField] List<Transform> _positions;
     public override void StartTask()
     {
-        throw new System.NotImplementedException();
+        foreach(GameObject player in PlayersDoingTask)
+        {
+            Transform pos = _positions[(int)Random.Range(0, _positions.Count)];
+            player.transform.position = pos.position;
+            player.GetComponent<PlayerController>().DisableMovementEnableInputs();
+            _positions.Remove(pos);
+        }
     }
 }
