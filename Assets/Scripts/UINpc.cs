98bb50ca186cc993a79653db37cb7bcd88cb0ba3
Alexis Le Bourhis <lebourhisalexis11@gmail.com>
2023-11-24 11:19:20 +0100

    Duolingo fix

--- Assets/Scripts/UINpc.cs
+++ Assets/Scripts/UINpc.cs
@@ -18,6 +18,7 @@ public class UINpc : MonoBehaviour
         _talkingBubbleImage = _talkingBubble.Find("Image").GetComponent<Image>();
         print(_talkingBubble.parent.parent.gameObject.name);
         _talkingBubbleText = _talkingBubble.Find("Text").GetComponent<TextMeshProUGUI>();
+        _talkingBubble.gameObject.SetActive(false);
         //print(_canvas + " " + _talkingBubble + " " + _talkingBubbleText + " "+ _talkingBubbleImage );
     }
 
